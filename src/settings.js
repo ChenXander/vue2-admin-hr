5403ab4aa1bcd3390e6e386fac73286a6977409c
ChenXander <cx1197257655@gmail.com>
2022-04-09 16:17:05 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Vue Admin Hr',
+  // 固定头部
+  fixedHeader: false,
+  // 显示左侧菜单logo
+  sidebarLogo: false
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,8 @@
+// 用户信息
+
+export default {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {}
+}
