35f1dfccb6276684b45a9051d81ba07dfd19412b
ChenXander <cx1197257655@gmail.com>
2022-04-11 19:59:16 +0800

    删除角色的功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -27,11 +27,21 @@
               />
               <el-table-column align="center" prop="description" label="描述" />
               <el-table-column align="center" label="操作">
-                <el-button size="small" type="success">分配权限</el-button>
-                <el-button size="small" type="primary">编辑</el-button>
-                <el-button size="small" type="danger">删除</el-button>
+                <!-- 作用域插槽 -->
+                <template slot-scope="{ row }">
+                  <el-button size="small" type="success">分配权限</el-button>
+                  <el-button size="small" type="primary">编辑</el-button>
+                  <el-button
+                    size="small"
+                    type="danger"
+                    @click="deleteRole(row.id)"
+                  >
+                    删除
+                  </el-button>
+                </template>
               </el-table-column>
             </el-table>
+
             <!-- 分页组件 -->
             <el-row
               type="flex"
@@ -49,6 +59,7 @@
               />
             </el-row>
           </el-tab-pane>
+
           <el-tab-pane label="公司信息">
             <el-alert
               title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
@@ -96,7 +107,7 @@
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { getRoleList, getCompanyInfo, deleteRole } from '@/api/setting'
 
 import { mapGetters } from 'vuex'
 
@@ -135,6 +146,20 @@ export default {
       // newPage是当前点击的页码
       this.page.page = newPage // 将当前页码赋值给当前的最新页码
       this.getRoleList()
+    },
+    // 删除角色
+    async deleteRole (id) {
+      try {
+        //  提示
+        await this.$confirm('确认删除该角色吗')
+        // 只有点击了确定 才能进入到下方
+        await deleteRole(id) // 调用删除接口
+
+        this.getRoleList() // 重新加载数据
+        this.$message.success('删除角色成功')
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
