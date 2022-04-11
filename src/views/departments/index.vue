a0ea28a27ea64bfbe7b0a40f7a1a7e5dc00953e7
ChenXander <cx1197257655@gmail.com>
2022-04-11 10:33:23 +0800

    组织架构头部

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,38 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <!-- 组织架构头部 -->
+      <el-card class="tree-card">
+        <!-- 行列布局 -->
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px"
+        >
+          <!-- 左侧 -->
+          <el-col>
+            <span>HR科技股份有限公司</span>
+          </el-col>
+
+          <el-col :span="4">
+            <!-- 右侧 -->
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span>操作<i class="el-icon-arrow-down" /></span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
@@ -15,4 +46,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
