20351b9b348da5e568850957ab45c70579375e93
ChenXander <cx1197257655@gmail.com>
2022-04-12 14:02:50 +0800

    员工导入组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,12 @@
 // 所有的公共的组件的全局注册
 import PageTools from './PageTools'
+import UploadExcel from './UploadExcel'
+
 export default {
   install (Vue) {
     //  注册全局的通用栏组件对象
     Vue.component('PageTools', PageTools)
+    // 注册导入excel组件
+    Vue.component('UploadExcel', UploadExcel)
   }
 }
