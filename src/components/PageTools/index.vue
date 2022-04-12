49eb257431effd17e3c4f473160e842ac06387fb
ChenXander <cx1197257655@gmail.com>
2022-04-12 09:05:43 +0800

    封装一个通用的工具栏

---
+++ src/components/PageTools/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- 通用栏组件 -->
+  <el-card class="page-tools">
+    <el-row type="flex" justify="space-between" align="middle">
+      <el-col>
+        <!-- 左侧图标按需显示 -->
+        <div v-if="showBefore" class="before">
+          <i class="el-icon-info" />
+          <!-- 定义前面得插槽 -->
+          <slot name="before" />
+        </div>
+      </el-col>
+      <el-col>
+        <el-row type="flex" justify="end">
+          <!-- 定义后面的插槽 -->
+          <slot name="after" />
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: {
+    showBefore: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-tools {
+  margin: 10px 0;
+
+  .before {
+    line-height: 34px;
+    display: inline-block;
+    padding: 0px 10px;
+    border-radius: 3px;
+    border: 1px solid rgba(145, 213, 255, 1);
+    background: rgba(230, 247, 255, 1);
+
+    i {
+      margin-right: 5px;
+      color: #409eff;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 import Vue from 'vue'
-
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-
-import '@/styles/index.scss' // global css
-
 import App from './App'
 import store from './store'
 import router from './router'
+import Component from '@/components' // 全局通用组件
+/**
+ * elementUI
+ */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+/**
+ * 样式
+ */
+import '@/styles/index.scss' // global css
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
+/**
+ * 自定义指令
+ */
 import * as directives from '@/directives'
+/**
+ * 图标
+ */
 import '@/icons' // icon
 import '@/permission' // permission control
 
@@ -20,8 +29,8 @@ import '@/permission' // permission control
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+Vue.use(Component) // 注册自己的插件
 
-// 注册自定义指令
 // 遍历所有的导出的指令对象 完成自定义全局注册
 Object.keys(directives).forEach((key) => {
   Vue.directive(key, directives[key]) // 注册自定义指令
