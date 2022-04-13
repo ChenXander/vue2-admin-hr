17afa8e66be0398b711d219a4e42daf4411c2e06
ChenXander <cx1197257655@gmail.com>
2022-04-13 13:44:16 +0800

    打印员工信息

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
+import Print from 'vue-print-nb'
 
 export default {
   install (Vue) {
@@ -11,5 +12,7 @@ export default {
     Vue.component('UploadExcel', UploadExcel)
     // 注册导入上传组件
     Vue.component('ImageUpload', ImageUpload)
+    // 打印插件
+    Vue.use(Print)
   }
 }
